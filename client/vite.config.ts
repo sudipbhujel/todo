9fbe10b57c9997d9a5c5dd95be4c4b8cffd18028
Sudip Bhujel <devsudipbhujel@gmail.com>
2023-01-26 15:09:49 +0545

    feat(dockerfile): update dockerfile of backend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
-  preview: { port: 3000 },
+  preview: { port: 3000, host: true },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
